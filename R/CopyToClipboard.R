4c3a3865f09ed4d1d0263a978f43a05d9a997731
SelinaZitrone <selina.baldauf@gmx.de>
2020-10-05 22:24:37 +0200

    delete unnecessary files

--- R/CopyToClipboard.R
+++
@@ -1,7 +0,0 @@
-# file (string): file to be copied to the clipboard (with fileending)
-# filepath (string): path where file to be copied to clipboard is stored
-
-CopyToClipboard <- function(file, filepath = here::here("HTML")){
-  readLines(paste0(filepath, "/", file)) %>% clipr::write_clip()
-  #xml2::read_html(paste0(filepath, "/", file)) %>% clipr::write_clip()
-}
